dabac6e87ef6160abf11694fa0aa38410913f34c
sajangrover <sajangrover379@gmail.com>
2021-01-15 19:01:36 +0530

    add some custom events for message

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -1,19 +1,49 @@
 import React, {Component} from 'react'
 import queryString from 'query-string'
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:9000";
+let socket;
 class Chat extends Component {
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            message : ''
+        }
+    }
 
     componentDidMount() {
         let user = queryString.parse(this.props.location.search)
         console.log(user)
+        socket = socketIOClient(ENDPOINT);
+
+        socket.emit('join', user, (welcome) => {
+            console.log(welcome)
+        });
+        socket.on("message", data => {
+            console.log(data);
+        });
+
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            message : e.target.value
+        })
+    }
+    handleClick = () => {
+        socket.emit('sendMessage', this.state.message);
+        this.setState({
+            message : ''
+        })
     }
     render(){
         return (
             <div className="outerContainer">
                 <div className="innerContainer">
                     <div className="sendMessageContainer">
-                        <input id="message" type="text"/>
-                        <button id="send">send</button>
+                        <input id="message" type="text" onChange ={this.handleChange} value={this.state.message}/>
+                        <button id="send" onClick = {this.handleClick}>send</button>
                     </div>
                 </div>
             </div>
--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const socketIo = require("socket.io");
 const hostname = 'localhost';
 const port = 9000;
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+
 
 let user = {};
+const server = http.createServer(app);
+const io  = socketIo(server,{
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+      }
+});
+app.use(cors());
 app.post('/user', (req,res,next) => {
     if(user[req.body.userName] == req.body.room) {
         res.status(400).json({error: "User with this name allready exist in this room"});
@@ -17,6 +28,16 @@ app.post('/user', (req,res,next) => {
     res.status(200).json({message: "User registered successfully"});
 })
 
-app.listen(port,()=>{
+
+io.on('connect', socket => {
+     socket.on('join', (user, callback) => {
+        callback(`Welcome ${user.name} on ${user.room} room`);
+     })
+     socket.emit('message', "hello world");
+     socket.on('sendMessage',(msg) => {
+         console.log(msg)
+     })
+})
+server.listen(port,()=>{
     console.log(`server running at http://${hostname}:${port}`);
 })
